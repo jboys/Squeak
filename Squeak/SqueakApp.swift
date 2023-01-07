ff2531982629348c61ac1e3492826e4c1771f4e5
Josh Boys <josh.boys@gmail.com>
2023-01-07 13:06:12 +1030

    Initial Commit

---
+++ Squeak/SqueakApp.swift
@@ -0,0 +1,17 @@
+//
+//  SqueakApp.swift
+//  Squeak
+//
+//  Created by Josh Boys on 7/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct SqueakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
